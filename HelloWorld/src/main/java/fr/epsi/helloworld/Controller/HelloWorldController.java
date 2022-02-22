140438fab7c9c75b5b5056d28c3d1d58502b96cb
Kubilay6634 <72594889+Kubilay6634@users.noreply.github.com>
2022-02-22 12:03:36 +0100

    Add files via upload

---
+++ HelloWorld/src/main/java/fr/epsi/helloworld/Controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.epsi.helloworld.Controller;
+
+/**
+ *
+ * @author cetin
+ */
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(
+            path = "/hello",
+            produces = "text/plain")
+    public String getHelloWorld()
+    {
+        return "Hello World !";
+    }
+    
+}
