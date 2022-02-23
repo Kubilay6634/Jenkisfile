03eb068d2c16162a01c28a0847de32800403c6ce
Kubilay6634 <Vous@exemple.com>
2022-02-23 12:35:28 +0100

    comit

---
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(
+        path = "/hello",
+        produces = "text/plain"
+    )
+    public String getHelloWorld(){
+        return "Hello Woooorrrlldd !";
+    }
+}	
